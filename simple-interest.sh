6d50b4c20b6d0b5d6d1dcaeaf51de3e5a9ffa0ea
Jelvan Ricolcol <bolekapp@jelvan.pro>
2026-05-12 22:11:58 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,32 @@
 
+#!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
+
+# Author:hr-ai-full-stack-developer
+# Additional Authors: Jelvanm Ricolcol
+# IBMlearner2561
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r/100
+
+echo "Enter the principal:"
+read p
+
+echo "Enter rate of interest per year:"
+read r
+
+echo "Enter time period in years:"
+read t
+
+s=$(expr $p \* $t \* $r / 100)
+
+echo "The simple interest is:"
+echo $s
+```
